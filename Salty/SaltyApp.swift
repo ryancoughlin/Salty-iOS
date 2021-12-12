b62cab8925305c7be3a435b76cb5346abeb5a63a
ryancoughlin <ryan@ryancoughlin.com>
2021-12-11 19:28:22 -0500

    Initial Commit

---
+++ Salty/SaltyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaltyApp.swift
+//  Salty
+//
+//  Created by Ryan Coughlin on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SaltyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
